0e99fc910bb40d0f315fe03dbbc9791e34e40b59
Ryan889 <rjalbert@uark.edu>
2015-10-08 14:32:22 -0500

    Create TransactionEntryFieldNames.java

---
+++ GazelleModels/src/org/gazelle/models/fieldnames/TransactionEntryFieldNames.java
@@ -0,0 +1,7 @@
+import org.npc.dataaccess.model.BaseFieldNames;
+
+public class TransactionEntryFieldNames extends BaseFieldNames {
+	public static final String PRICE = "price";
+	public static final String QUANTITY = "quantity";
+	public static final String CREATED_ON = "createdon";
+}
