99e938848a9ecfdd138c122b8baffe98908a1137
Ryan889 <rjalbert@uark.edu>
2015-10-12 12:56:29 -0500

    Create TransactionEntryRepositoryInterface.java

---
+++ GazelleModels/src/org/gazelle/repositories/interfaces/TransactionEntryRepositoryInterface.java
@@ -0,0 +1,7 @@
+package org.gazelle.repositories.interfaces;
+
+import org.gazelle.models.TransactionEntry;
+import org.npc.dataaccess.repository.BaseRepositoryInterface;
+
+public interface TransactionEntryRepositoryInterface extends BaseRepositoryInterface<TransactionEntry> {
+}
