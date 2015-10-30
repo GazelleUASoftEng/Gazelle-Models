b3b173c76e023e4267aeb67753105872a2d332f6
jcalumba <jcalumba@email.uark.edu>
2015-10-29 21:10:34 -0400

    Calvin: replaced some outdated files, should compile now.

--- GazelleModels/src/org/gazelle/models/TenderEntry.java
+++ GazelleModels/src/org/gazelle/models/TenderEntry.java
@@ -6,77 +6,118 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import org.gazelle.enums.EmployeeClassification;
+import org.gazelle.enums.Paytype;
+import org.gazelle.models.fieldnames.EmployeeFieldNames;
+import org.gazelle.models.fieldnames.ProductFieldNames;
 import org.gazelle.models.fieldnames.TenderEntryFieldNames;
+import org.gazelle.models.fieldnames.TransactionFieldNames;
 import org.gazelle.repositories.TenderEntryRepository;
 import org.npc.dataaccess.model.BaseModel;
+import java.util.UUID;
+
 
 public class TenderEntry extends BaseModel<TenderEntry> {
 
+	private UUID record_id;
+	private double amount;
+	private Paytype paytype;
+	private LocalDateTime time;
+	private UUID transactionid;
+	
 	@Override
 	protected void fillFromRecord(ResultSet rs) throws SQLException {
-		this.price = rs.getFloat(TenderEntryFieldNames.PRICE);
-		this.quantity = rs.getInt(TransactionEntryFieldNames.QUANTITY);
-		this.createdon = rs.getTimestamp(TransactionEntryFieldNames.CREATED_ON).toLocalDateTime();
 
-		this.recordid = rs.getFloat(TenderEntryFieldNames.recordid);
-		this.amount = rs.getFloat(TenderEntryFieldNames.amount);
-		this.price = rs.getFloat(TenderEntryFieldNames.paytype);
-		this.price = rs.getFloat(TenderEntryFieldNames.time);
-		this.price = rs.getFloat(TenderEntryFieldNames.transactionid);
+		this.record_id = (UUID)rs.getObject(TenderEntryFieldNames.record_id); // make
+		this.amount = rs.getDouble(TenderEntryFieldNames.amount);		// make
+//		this.paytype = rs.getFloat(TenderEntryFieldNames.paytype);		// make
+		this.paytype = Paytype.map(rs.getInt(TenderEntryFieldNames.paytype));
+		this.time = rs.getTimestamp(TenderEntryFieldNames.time).toLocalDateTime();
+//		this.time = rs.getFloat(TenderEntryFieldNames.time);			// make
+		this.transactionid = (UUID)rs.getObject(TenderEntryFieldNames.transactionid);
+//		this.transactionid = rs.getFloat(TenderEntryFieldNames.transactionid);	// make
 
 	}
 	
 	protected Map<String, Object> fillRecord(Map<String, Object> record) {
-		record.put(TransactionEntryFieldNames.PRICE, this.price);
-		record.put(TransactionEntryFieldNames.QUANTITY, this.quantity);
-		
-		record.put(TransactionEntryFieldNames.CREATED_ON, Timestamp.valueOf(this.createdon));
+		record.put(TenderEntryFieldNames.paytype, this.paytype);
+		record.put(TenderEntryFieldNames.amount, this.amount);
+		record.put(TenderEntryFieldNames.record_id, this.record_id);
+		record.put(TenderEntryFieldNames.time, this.time);
+		record.put(TenderEntryFieldNames.transactionid, this.transactionid);
 	
 		return record;
 	}
 	
-	private double price;
-	public double getPrice() {
-		return this.price;
+	public UUID getRecID(){
+		return this.record_id;
 	}
-	public TenderEntry setPrice(double price) {
-		if (this.price != price) {
-			this.price = price;
-			this.propertyChanged(TransactionEntryFieldNames.PRICE);
-		}
-		return this;
-	}
-	
-	private int quantity;
-	public int getQuantity() {
-		return this.quantity;
-	}
-	
-	public TenderEntry setQuantity(int quantity) {
-		if (this.quantity != quantity) {
-			this.quantity = quantity;
-			this.propertyChanged(TransactionEntryFieldNames.QUANTITY);
-		}
-		return this;
-	}
-	
-	private LocalDateTime createdon;
-	public LocalDateTime getCreatedOn() {
-		return this.createdon;
-	}
-	public TenderEntry setCreatedOn(LocalDateTime createdon){
-		if (this.createdon != createdon){
-			this.createdon = createdon;
-			this.propertyChanged(TransactionEntryFieldNames.CREATED_ON);
-		}
-		return this;
-	}
-	
-	public TenderEntry() {
-		super(new TenderEntry());
-		this.price = 0;
-		this.quantity = 0;
-		this.time = LocalDateTime.now();
-	}
-}
 
+	public double getAmount(){
+		return this.amount;
+	}
+		
+	public LocalDateTime getTime() {
+		return this.time;  
+	}
+
+	public Paytype getPaytype(){
+		return this.paytype;
+	}
+
+	public UUID getTransID(){
+		return this.transactionid;
+	}
+
+	public TenderEntry setRecID(UUID input){
+		if(this.record_id != input)
+		{
+			this.record_id = input;
+			this.propertyChanged(TenderEntryFieldNames.record_id);
+		}
+		return this;
+	}
+
+	public TenderEntry setAmount(double input){
+		if(this.amount != input)
+		{
+			this.amount = input;
+			this.propertyChanged(TenderEntryFieldNames.amount);
+		}
+		return this;
+	}
+	public TenderEntry setTime(LocalDateTime input){
+		if(this.time != input)
+		{
+			this.time = input;
+			this.propertyChanged(TenderEntryFieldNames.time);
+		}
+		return this;
+	}
+	public TenderEntry setPaytype(Paytype input){
+		if(this.paytype != input)
+		{
+			this.paytype = input;
+			this.propertyChanged(TenderEntryFieldNames.paytype);
+		}
+		return this;
+	}
+	public TenderEntry setTransID(UUID input){
+		if(this.transactionid != input)
+		{
+			this.transactionid = input;
+			this.propertyChanged(TenderEntryFieldNames.transactionid);
+		}
+		return this;
+	}
+
+	public TenderEntry() {
+		super(new TenderEntryRepository());
+		this.record_id = new UUID (0, 0);
+		this.amount = 0.0;
+		this.time = LocalDateTime.now();
+		this.paytype = null;
+		this.transactionid = new UUID (0, 0);
+	}
+
+}
--- GazelleModels/src/org/gazelle/repositories/interfaces/TenderEntryRepositoryInterface.java
+++ GazelleModels/src/org/gazelle/repositories/interfaces/TenderEntryRepositoryInterface.java
@@ -1,7 +1,5 @@
 package org.gazelle.repositories.interfaces;
 
-import org.gazelle.models.TenderEntry;
-import org.npc.dataaccess.repository.BaseRepositoryInterface;
+public class TenderEntryRepositoryInterface {
 
-public interface TenderEntryRepositoryInterface extends BaseRepositoryInterface<TenderEntry> {
 }
