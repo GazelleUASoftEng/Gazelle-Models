da0a7c5cbaffd3ad85e1caaf656e41b45caaa0e6
Ryanjalbertson <ryanjalbertson@gmail.com>
2015-10-30 16:44:04 -0500

    Added Field Names for transactionentry

--- GazelleModels/src/org/gazelle/models/fieldnames/TransactionEntryFieldNames.java
+++ GazelleModels/src/org/gazelle/models/fieldnames/TransactionEntryFieldNames.java
@@ -3,7 +3,10 @@ package org.gazelle.models.fieldnames;
 import org.npc.dataaccess.model.BaseFieldNames;
 
 public class TransactionEntryFieldNames extends BaseFieldNames {
+	public static final String RECORD_ID = "record_id";
 	public static final String PRICE = "price";
 	public static final String QUANTITY = "quantity";
 	public static final String CREATED_ON = "createdon";
+	public static final String TRANSACTION_ID = "transaction_id";
+	public static final String PRODUCT_ID = "product_id";
 }
