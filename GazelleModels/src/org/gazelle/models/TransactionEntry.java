8d132e2ffa31e3955bbb95970f86879e951c9efb
Ryanjalbertson <ryanjalbertson@gmail.com>
2015-10-30 16:44:55 -0500

    added constructor

--- GazelleModels/src/org/gazelle/models/TransactionEntry.java
+++ GazelleModels/src/org/gazelle/models/TransactionEntry.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.Map;
+import java.util.UUID;
 
 import org.gazelle.models.fieldnames.TransactionEntryFieldNames;
 import org.gazelle.repositories.TransactionEntryRepository;
@@ -63,8 +64,48 @@ public class TransactionEntry extends BaseModel<TransactionEntry> {
 		return this;
 	}
 	
+	private UUID record_id;
+	public UUID getRecordID() {
+		return this.record_id;
+	}
+	
+	public TransactionEntry setRecordID(UUID record_id) {
+		if (this.record_id != record_id) {
+			this.record_id = record_id;
+			this.propertyChanged(TransactionEntryFieldNames.RECORD_ID);
+		}
+		return this;
+	}
+	
+	private UUID product_id;
+	public UUID getProductID() {
+		return this.product_id;
+	}
+	
+	public TransactionEntry setProductID(UUID product_id) {
+		if (this.product_id != product_id) {
+			this.product_id = product_id;
+			this.propertyChanged(TransactionEntryFieldNames.PRODUCT_ID);
+		}
+		return this;
+	}
+	
+	private UUID transaction_id;
+	public UUID getTransactionID() {
+		return this.transaction_id;
+	}
+	
+	public TransactionEntry setTransactionID(UUID transaction_id) {
+		if (this.transaction_id != transaction_id) {
+			this.transaction_id = transaction_id;
+			this.propertyChanged(TransactionEntryFieldNames.TRANSACTION_ID);
+		}
+		return this;
+	}
+	
 	public TransactionEntry() {
 		super(new TransactionEntryRepository());
+		this.record_id = new UUID(0, 0);
 		this.price = 0;
 		this.quantity = 0;
 		this.createdon = LocalDateTime.now();
